62868a57364ef63e2b2bf61b6a88cc4af5e13193
Toby Tripp <toby.tripp+github@gmail.com>
2012-06-04 22:44:04 -0500

    Ajax success!

--- public/js/contact-list/models/contact-collection.js
+++ public/js/contact-list/models/contact-collection.js
@@ -2,5 +2,9 @@
   ContactList.ContactCollection = Backbone.Collection.extend({
     url: "/contacts",
     model: ContactList.Contact,
+
+    parse: function( response ) {
+      return response.contacts;
+    }
   });
 }(undefined));
