cb1c13d18f3cbd7373e1d7ea2725441a3ee3e0c6
Toby Tripp <toby.tripp+github@gmail.com>
2012-06-04 21:10:21 -0500

    Naïve implementation of the list view.

--- public/js/contact-list/views/list-view.js
+++ public/js/contact-list/views/list-view.js
@@ -1,5 +1,13 @@
 ;(function($) {
   ContactList.ListView = Backbone.View.extend({
+    tagName:   "div",
+    className: "js-contact-list",
 
+    initialize: function( options ) {},
+
+    render: function() {
+      this.$el.html( "<div class='js-contact-list'></div>" );
+      return this;
+    }
   });
 }(jQuery));
