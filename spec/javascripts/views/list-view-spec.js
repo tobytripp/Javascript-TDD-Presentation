22d63585ba45b2a3611aa2a809ea639bf974a288
Toby Tripp <toby.tripp+github@gmail.com>
2012-06-04 21:37:15 -0500

    Start toward the contact view.

--- spec/javascripts/views/list-view-spec.js
+++ spec/javascripts/views/list-view-spec.js
@@ -1,25 +1,38 @@
 describe( "ListView", function() {
   var view;
+  var contacts;
+
   beforeEach( function() {
     loadFixtures( "list.html" );
+    contacts = new ContactList.ContactCollection();
+    view = new ContactList.ListView({ model: contacts, el: $("#contact-list") });
   });
 
   describe( "#render", function() {
     beforeEach( function() {
-      var contacts = new ContactList.ContactCollection();
-      view = new ContactList.ListView({ model: contacts, el: $("#contact-list") });
-
+      // Over-ride the template from the fixture
       view.template = $("#list-template").html();
       view.render();
     });
 
-    it( "loads the fixture", function() {
-      expect( $("#contact-list") ).toExist();
-    });
-
-
     it( "creates a div container", function() {
       expect( $("div.js-contact-list") ).toBeVisible();
+    });
+  });
+
+  describe( "when the collection adds an element", function() {
+    var newContact;
+
+    beforeEach( function() {
+      newContact = { name: 'Bob' };
+    });
+
+    it( "creates a ContactView for the new element", function() {
+      spyOn( ContactList, "ContactView" );
+
+      contacts.trigger( 'add', newContact );
+
+      expect( ContactList.ContactView ).toHaveBeenCalledWith({ model: newContact });
     });
   });
 });
