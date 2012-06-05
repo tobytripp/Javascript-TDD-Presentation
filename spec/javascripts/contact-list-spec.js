a4513ca7285da9f5a5d7114a5160088b2b456b9c
Toby Tripp <toby.tripp+github@gmail.com>
2012-06-04 20:15:11 -0500

    Failure with a Jasmine Spy

--- spec/javascripts/contact-list-spec.js
+++ spec/javascripts/contact-list-spec.js
@@ -2,4 +2,14 @@ describe( "ContactList", function() {
   it( "accepts a container element in its constructor", function() {
     new ContactList( $("#my-contact-list") );
   });
+
+  it( "creates a ContactListController with the element", function() {
+    var el = $("#my-contact-list");
+    spyOn( ContactList, "ContactListController" );
+
+    new ContactList( el );
+
+    expect( ContactList.ContactListController ).toHaveBeenCalledWith( el );
+  });
+
 });
