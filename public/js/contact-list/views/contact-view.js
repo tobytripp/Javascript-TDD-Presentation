be5b232680b92b43f6ccae16b92ed4430e8131e2
Toby Tripp <toby.tripp+github@gmail.com>
2012-06-05 19:02:23 -0500

    Contact View.

--- public/js/contact-list/views/contact-view.js
+++ public/js/contact-list/views/contact-view.js
@@ -1,5 +1,15 @@
 ;(function($) {
   ContactList.ContactView = Backbone.View.extend({
-  
+    tagName: "div",
+    className: "vcard",
+
+    initialize: function() {
+      this.template = _.template( $("#card-template").html() );
+    },
+
+    render: function() {
+      this.$el.html( this.template( this.model.toJSON() ) );
+      return this;
+    }
   });
 }(jQuery));
--- spec/javascripts/views/contact-view-spec.js
+++ spec/javascripts/views/contact-view-spec.js
@@ -1,6 +1,32 @@
 describe( "ContactList.ContactView", function() {
-  it( "needs to be written", function() {
-    expect( "fail" ).toBeFalsey();
+  var data = {
+    "name": { "first": "Sim", "last": "Wyman" },
+    "url": "http://www.schaefer.biz.biz",
+    "email": "selena@sanford.info",
+    "address": {
+      "streetAddress":"4679 Leanne Branch Apt. 330",
+      "city":"East Dedrick",
+      "state":"Connecticut","zip":"50962"
+    },
+    "phone": "1-237-138-5650 x1243",
+    "jabber": "aida@ondricka.biz"
+  }
+  var contact, view;
+
+  beforeEach( function() {
+    loadFixtures( "list.html" );
+    contact = new ContactList.Contact( data );
+    view    = new ContactList.ContactView({ model: contact });
+
+    $("#contact-list").append( view.render().el );
   });
 
+  it( "wraps its content in a vcard container", function() {
+    expect( $(".vcard") ).toBeVisible();
+  });
+
+
+  it( "renders the model into its template", function() {
+    expect( $(".vcard p.name") ).toHaveText( data.name.first + " " + data.name.last );
+  });
 });
